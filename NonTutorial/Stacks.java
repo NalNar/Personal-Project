17271a00a0378928255173944252492d11a43635
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2025-11-16 18:13:57 +0000

    CLI Expense APP

---
+++ NonTutorial/Stacks.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+/**
+ * linked list
+ * 
+ * 
+
+queue 
+
+priority
+*/
+public class Stacks{
+    public static void main(String[]args){
+        // stack of Integer objects
+        Stack<String> stack = new Stack<String>();
+
+        String s = "YOYOYO" ;
+        stack.pop();
+        System.out.println(stack);
+
+
+    }
+}
